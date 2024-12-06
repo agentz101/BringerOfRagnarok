e61ef7bb22adb34935e365e55e9280c6683658e2
IvanChingUNR <chingnavi88@gmail.com>
2024-12-05 21:44:03 -0800

    ranged enemy prototype

---
+++ Assets/Projectile.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed;
+        private Transform player;
+        private Vector3 target;
+        // Start is called before the first frame update
+        void Start()
+        {
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+            target = new Vector3(player.position.x, player.position.y, player.position.z);
+        }
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+            if(transform.position.x == target.x && transform.position.y == target.y && transform.position.z == target.z){
+                DestroyProjectile();
+            }
+        
+        }
+
+
+        void OnTriggerEnter(Collider other){
+            if(other.CompareTag("Player")){
+                DestroyProjectile();
+            }
+        }
+        void DestroyProjectile(){
+            Destroy(gameObject);
+        }
+
+}
