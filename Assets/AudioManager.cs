d3fcbce2c7d8170052fec1af0f1316307fdb0778
IvanChingUNR <chingnavi88@gmail.com>
2024-12-05 21:28:50 -0800

    background music

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioSource musicSource;
+
+
+    public AudioClip background;
+    public static AudioManager instance;
+
+
+    private void Awake(){
+        if(instance ==  null){
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else{
+            Destroy(gameObject);
+        }
+    }
+
+
+    private void Start(){
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+}
